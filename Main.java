d140f40751db394f4a511fefbb3230c16cf4d8a1
Taiki HARA <e165722@ie.u-ryukyu.ac.jp>
2016-12-17 18:01:59 +0900

    Report5

---
+++ Main.java
@@ -0,0 +1,11 @@
+package jp.ac.uryukyu.ie.e165722;
+
+/**
+ * Created by e165722 on 2016/12/17.
+ */
+public class Main {
+    public static void main(String[] args){
+        String str = null;
+        System.out.println(str.length());
+    }
+}
