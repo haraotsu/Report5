8f9f9b69abfd0a682618df253a99a28c358f1b2f
Taiki HARA <e165722@ie.u-ryukyu.ac.jp>
2016-12-17 18:28:06 +0900

    Report5

--- Main.java
+++ Main.java
@@ -6,7 +6,13 @@ package jp.ac.uryukyu.ie.e165722;
 public class Main {
     public static void main(String[] args){
         String str = null;
-        
-        System.out.println(str.length());
+
+        try{
+            System.out.println(str.length());
+        }catch(NullPointerException ex) {
+            System.out.println("NullPointerExceptionが発生しました");
+            ex.printStackTrace();
+
+        }
     }
 }
